4b12250bbbad536e1395faba492966079e587ff2
Big Mogician <sunaoran@youzan.com>
2019-06-25 13:58:01 +0800

    feat: remove src level and add pkg version help

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import commander from "commander";
+import { CompilePlugin } from "./actions/compile";
+import { ConfigPlugin } from "./actions/config";
+import { DevPlugin } from "./actions/dev";
+import { MiddlewarePlugin } from "./actions/middleware";
+import { RouterPlugin } from "./actions/routers";
+import { CommandPlugin } from "./base";
+
+// tslint:disable-next-line: no-var-requires
+const pkg = require("./package.json");
+
+function initCommand(plugin: CommandPlugin) {
+  const program = commander.name(plugin.name).description(plugin.description);
+  if (plugin.options) {
+    for (const plu of plugin.options) {
+      program.option(plu[0], plu[1]);
+    }
+  }
+  program.action(plugin.action).on("--help", plugin.help);
+}
+
+[DevPlugin, CompilePlugin, RouterPlugin, ConfigPlugin, MiddlewarePlugin].forEach(i => initCommand(i));
+
+commander.version(pkg.version || "1.0.0-rc.1").parse(process.argv);
+
+if (commander.args.length === 0) {
+  commander.outputHelp();
+}
